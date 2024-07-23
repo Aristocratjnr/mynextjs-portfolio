5e6cbfcc0c6a7dcf98da6d2571239b8ff4c6e5be
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-23 17:49:48 +0000

    Update image source in AboutSection component

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -54,7 +54,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/web.png" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
