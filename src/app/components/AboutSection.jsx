d70fd1c0a6e72b722ee8f823b1b3eaba3e99821e
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-23 21:08:49 +0000

    Update image source in AboutSection component

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -54,13 +54,13 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/web.png" width={500} height={500} />
+        <Image src="/images/web.jpg" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
             I am a aspirant full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, python, Next.js, sql, HTML, CSS, and Git. I am a quick learner and I am always
+            working with JavaScript, React, python, Next.js, Sql, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
