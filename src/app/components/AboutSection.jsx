f736d30e8511f46be7c695e97ed44ab9ba308c2f
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-23 16:56:05 +0000

    Update skills list and educational background in AboutSection component

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -23,8 +23,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>Information Technology Student</li>
+        <li>University of Ghana</li>
       </ul>
     ),
   },
@@ -33,8 +33,9 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>Intermediate Python in Data Science</li>
+        <li>Data Analysis with Python, IBM</li>
+        <li>Introduction to Artificial Intelligence with Honours,IBM</li>
       </ul>
     ),
   },
@@ -57,10 +58,9 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
+            I am a aspirant full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+            working with JavaScript, React, python, Next.js, sql, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
