78c5f238dc880d907df5272203a5df9984a6f972
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-26 23:18:38 +0000

    Update project image and description in ProjectsSection component

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -52,9 +52,9 @@ const projectsData = [
   },
   {
     id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
+    title: "Portfolio Sample",
+    description: "My first portfolio webpage", 
+    image: "/images/projects/port.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
