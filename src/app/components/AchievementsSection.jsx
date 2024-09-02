056a613b0cc2bc2934b445d56eb21c758273cbce
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-09-02 13:46:12 +0000

    chore: Update AchievementsSection with new metrics and values

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -12,13 +12,13 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: "40",
+    value: "45",
     postfix: "+",
   },
   {
     prefix: "~",
-    metric: "Users",
-    value: "100,000",
+    metric: "LinkedIn Connections",
+    value: "500,000",
   },
   {
     metric: "Awards",
